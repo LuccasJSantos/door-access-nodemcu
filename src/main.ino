47f0b2b0c45f09e14e89851d302418d3d21f8eab
Lael Jader <lael.jader@lab2dev.com>
2022-09-30 13:25:20 -0300

    feat(*): integration with display module

--- src/main.ino
+++ src/main.ino
@@ -14,101 +14,32 @@
 constexpr uint8_t RFID_RST_PIN = D3;     // Configurable, see typical pin layout above
 constexpr uint8_t RFID_SDA_PIN = D4;     // Configurable, see typical pin layout above
 
-#define DISPLAY_CS      13
-#define DISPLAY_DC      2
-#define DISPLAY_SDA     13
-#define DISPLAY_SCK     14
-#define DISPLAY_RST     3
+constexpr uint8_t DISPLAY_CS = D8;
+constexpr uint8_t DISPLAY_DC = D2; // A0/DC;
+constexpr uint8_t DISPLAY_RST = D1;
 
-#define SIZE_BUFFER     18
-#define MAX_SIZE_BLOCK  16
-#define PIN_AP          16
+constexpr uint8_t DISPLAY_SDA = D7; // sda/mosi;
+constexpr uint8_t DISPLAY_SCK = D5;
 
-Adafruit_ST7735 tft = Adafruit_ST7735(DISPLAY_CS, DISPLAY_DC, DISPLAY_SDA, DISPLAY_SCK, DISPLAY_RST);
+Adafruit_ST7735 display = Adafruit_ST7735(DISPLAY_CS,  DISPLAY_DC, DISPLAY_RST); 
 
 MFRC522 rfid(RFID_SDA_PIN, RFID_RST_PIN); // Instance of the class
 MFRC522::MIFARE_Key key;
 
-int LINHA_DO_DISPLAY = 0;
-
-// callback que indica que o ESP entrou no modo AP
-void configModeCallback (WiFiManager *myWiFiManager) {  
-  // Serial.println("Entered config mode");
-  Serial.println("Entrou no modo de configuração");
-  Serial.println(WiFi.softAPIP()); // imprime o IP do AP
-  Serial.println(myWiFiManager->getConfigPortalSSID()); // imprime o SSID criado da rede
-}
-
-//callback que indica que salvamos uma nova rede para se conectar (modo estação)
-void saveConfigCallback () {
-  Serial.println("Configuração salva");
-}
-
-void mensagensIniciais()
-{
-  // Mensagens iniciais no serial monitor
-  Serial.println("Aproxime o seu cartao do leitor...");
-  Serial.println();
-
-  // Mensagens iniciais no display
-  tft.fillScreen(ST77XX_BLACK); //Pinta a tela de preto
-  LINHA_DO_DISPLAY = 1;
-  displayPrint("Door Access", ST77XX_WHITE);
-  LINHA_DO_DISPLAY = 3;
-  displayPrint("Aproxime seu cartao", ST77XX_WHITE);
-  displayPrint("do leitor...", ST77XX_WHITE);
-  delay(1000);
-}
-
-//função para controlar linhas e automatizar impressão no display
-void displayPrint(char *text, uint16_t color) {
-  tft.setCursor(0, LINHA_DO_DISPLAY * 10);
-  tft.setTextColor(color);
-  tft.setTextWrap(true);
-  tft.print(text);
-  LINHA_DO_DISPLAY++;
-  if (LINHA_DO_DISPLAY > 12)
-  {
-    tft.fillScreen(ST77XX_BLACK); //Pinta a tela de preto
-    LINHA_DO_DISPLAY = 0;
-  }
-}
-
-String getCardId() {
-  String cardId = "";
-  for (byte i = 0; i < rfid.uid.size; i++) {
-    cardId.concat(String(rfid.uid.uidByte[i] < 0x10 ? "0" : "")); // Adiciona o zero se o id é menor que 16 ou seja 00, 01, 02, ..., 0E, 0F;
-    cardId.concat(String(rfid.uid.uidByte[i], HEX));
-  }
-  cardId.toUpperCase();
-  return cardId;
-}
-
-boolean isCardPresent() {
-  rfid.PICC_ReadCardSerial(); //Always fails
-  Serial.println(rfid.PICC_IsNewCardPresent());
-  Serial.println(rfid.PICC_ReadCardSerial());
-  if (rfid.PICC_IsNewCardPresent()) {
-    if (rfid.PICC_ReadCardSerial()) {
-      return true;
-    }
-  }
-  return false;
-}
+int currentDisplayLine = 0;
 
 void setup() {
   Serial.begin(9600);
+  
   SPI.begin();
 
-  // Use this initializer if using a 1.8" TFT screen:
-  //tft.initR(INITR_BLACKTAB);      // Init ST7735S chip, black tab
-  //tft.setRotation(1); //Ajusta a direção do display
-  //Serial.println(F("Display iniciado."));
+  displayInit();
 
   // Inicia MFRC522
   rfid.PCD_Init();
   
-  //mensagensIniciais();
+  mensagensIniciais();
+  
   /*
   // declaração do objeto wifiManager
   WiFiManager wifiManager;
@@ -126,25 +57,84 @@ void setup() {
   */
 }
 
-String tag;
-
 void loop() {
-  if (!rfid.PICC_IsNewCardPresent())
-    return;
-  if (rfid.PICC_ReadCardSerial()) {
-    for (byte i = 0; i < 4; i++) {
-      tag += rfid.uid.uidByte[i];
-    }
-    Serial.println(tag);
-    tag = "";
-    rfid.PICC_HaltA();
-    rfid.PCD_StopCrypto1();
+  if (isCardPresent()) {
+    String tag = getCardId();
 
-    /*
-    char * cardId;
-    tag.toCharArray(cardId, 32);
-    Serial.println(cardId);
-    displayPrint(cardId, ST77XX_WHITE);
-    */
+    Serial.println(tag);
+    
+    char *cardId = &tag[0];
+    
+    displayPrint(cardId, ST77XX_CYAN);
   }
+}
+
+// callback que indica que o ESP entrou no modo AP
+void configModeCallback(WiFiManager *myWiFiManager) {  
+  // Serial.println("Entered config mode");
+  Serial.println("Entrou no modo de configuração");
+  Serial.println(WiFi.softAPIP()); // imprime o IP do AP
+  Serial.println(myWiFiManager->getConfigPortalSSID()); // imprime o SSID criado da rede
+}
+
+//callback que indica que salvamos uma nova rede para se conectar (modo estação)
+void saveConfigCallback() {
+  Serial.println("Configuração salva");
+}
+
+void displayInit() {
+  display.initR(INITR_BLACKTAB); // Init ST7735S chip, black tab
+  display.setTextColor(ST7735_WHITE);
+  display.setRotation(1);
+  display.setTextWrap(true);
+
+  Serial.println(F("Display iniciado."));
+}
+
+void mensagensIniciais() {
+  // Mensagens iniciais no display
+  display.fillScreen(ST77XX_BLACK); //Pinta a tela de preto
+  currentDisplayLine = 1;
+  displayPrint("Door Access", ST77XX_GREEN);
+  currentDisplayLine = 3;
+  displayPrint("Aproxime seu cartao", ST77XX_WHITE);
+  displayPrint("do leitor...", ST77XX_WHITE);
+}
+
+//função para controlar linhas e automatizar impressão no display
+void displayPrint(char *text, uint16_t color) {
+  if (currentDisplayLine > 12) {
+    display.fillScreen(ST77XX_BLACK); //Pinta a tela de preto
+    currentDisplayLine = 0;
+  }
+  
+  display.setCursor(0, currentDisplayLine * 10);
+  display.setTextColor(color);
+  display.print(text);
+  
+  currentDisplayLine++;
+}
+
+String getCardId() {
+  String cardId = "";
+  for (byte i = 0; i < rfid.uid.size; i++) {
+    cardId += String(rfid.uid.uidByte[i] < 0x10 ? "0" : ""); // Adiciona o zero se o id é menor que 16 ou seja 00, 01, 02, ..., 0E, 0F;
+    cardId += String(rfid.uid.uidByte[i], HEX);
+  }
+  
+  rfid.PICC_HaltA();
+  rfid.PCD_StopCrypto1();
+    
+  cardId.toUpperCase();
+  
+  return cardId;
+}
+
+boolean isCardPresent() {
+  if (rfid.PICC_IsNewCardPresent()){
+    if (rfid.PICC_ReadCardSerial()) {
+      return true;
+    }
+  }
+  return false;
 }
